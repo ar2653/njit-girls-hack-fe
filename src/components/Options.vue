beab5742f82559c1acac5d5789cb1caa07f58a46
Shaik Tabrez <37013844+tabrezdn1@users.noreply.github.com>
2023-09-24 01:20:52 -0400

    Add ticket template

--- src/components/Options.vue
+++ src/components/Options.vue
@@ -38,7 +38,102 @@
         </div>
       </div>
     </div>
-    <div class="tickets">Get your ticket here!</div>
+    <div class="tickets">
+      <h4>Get your ticket here!</h4>
+      <button @click="fetchTickets()">Fetch!</button>
+      <div v-for="(ticket, index) in tickets" :key="index">
+        <div class="ticket">
+          <div class="left">
+            <div class="image">
+              <p class="admit-one">
+                <span
+                  ><img
+                    class="sponsor-img"
+                    src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQVj-3ZgTNfEZdmpaa1Fng2Iw_FvNQUQA5nmk4oBw7y&s"
+                    alt="planetInfo.displayName"
+                /></span>
+                <span
+                  ><img
+                    class="sponsor-img"
+                    src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSpGANoMqSG8nLVzrMdhuDcmb0UBTMf7f-Uk8x6fG918w&s"
+                    alt="planetInfo.displayName"
+                /></span>
+                <span
+                  ><img
+                    class="sponsor-img"
+                    src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRM5HoiIbFA5BvhKxZQSlzGRfEz4IxvSa0cIOCFTYEbZw&s"
+                    alt="planetInfo.displayName"
+                /></span>
+              </p>
+              <img
+                class="planet-img"
+                :src="`./assets/cards/${ticket.data.planet_image}.png`"
+                alt="planetInfo.displayName"
+              />
+              <div class="ticket-number">
+                <p>#{{ ticket.data._id.substring(0,6) }}</p>
+              </div>
+            </div>
+            <div class="ticket-info">
+              <p class="date">
+                <span>TUESDAY</span>
+                <span class="june-29">JUNE 29TH</span>
+                <span>2021</span>
+              </p>
+              <div class="show-name">
+                <h2>{{ticket.data.first_name}} {{ticket.data.last_name}}</h2>
+              </div>
+              <div class="time">
+                <p>EARTH<span> TO </span>{{ ticket.data.planet_image.toUpperCase() }}</p>
+                <p>Travelling <span>@</span> {{ ticket.data.package }} Package</p>
+              </div>
+              <p class="location">
+                <span>NJIT</span>
+                <span class="separator"><i class="far fa-smile"></i></span
+                ><span>GirlsHackathon 2023</span>
+              </p>
+            </div>
+          </div>
+          <div class="right">
+            <p class="admit-one">
+              <span
+                ><img
+                  class="sponsor-img"
+                  src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRMMqM-7Xc8732C_8vx6ze1HnNVo2Rw25dQPeo3kMNW0g&s"
+                  alt="planetInfo.displayName"
+              /></span>
+              <span
+                ><img
+                  class="sponsor-img"
+                  src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR36VDb-06OycfZuYqcWIZRReofQgoQprCRWF9V5c3BdQ&s"
+                  alt="planetInfo.displayName"
+              /></span>
+              <span
+                ><img
+                  class="sponsor-img"
+                  src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQy-b6LuMASfHQLzJE-eo9vr12Vh6vDkRSTZf3dup2A&s"
+                  alt="planetInfo.displayName"
+              /></span>
+            </p>
+            <div class="right-info-container">
+              <div class="time">
+                <p>EARTH <span></span></p>
+                <p><span>TO</span></p>
+                <p>{{ ticket.data.planet_image.toUpperCase() }} </p>
+                <p><span>@</span>{{ ticket.data.package }} </p>
+              </div>
+              <div class="barcode">
+                <img
+                  src="https://external-preview.redd.it/cg8k976AV52mDvDb5jDVJABPrSZ3tpi1aXhPjgcDTbw.png?auto=webp&s=1c205ba303c1fa0370b813ea83b9e1bddb7215eb"
+                  alt="QR code"
+                />
+              </div>
+              <p class="ticket-number">{{ ticket.data.price }} ETH</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -48,6 +143,21 @@ export default {
     return {
       speed: "day_sec",
       playMusic: true,
+      tickets: [
+        {
+          data: {
+            _id: "650fbe44c7b6e445dc869e19",
+            first_name: "abhi",
+            last_name: "shek",
+            cosmic_handle: "nanana",
+            appointment_date: "2023-09-27T00:00:00.000Z",
+            package: "Premium",
+            price: "12998.7",
+            __v: 0,
+            planet_image: "sun"
+          },
+        },
+      ],
     };
   },
   emits: ["speedChanged"],
@@ -60,6 +170,9 @@ export default {
       this.playMusic = !this.playMusic;
       this.$emit("musicUpdate", this.playMusic);
     },
+    fetchTickets() {
+    //   this.showTicket = true;
+    },
   },
   mounted() {
     this.$emit("speedChanged", this.speed);
@@ -71,14 +184,211 @@ export default {
 label {
   font-family: "Orbitron", sans-serif;
 }
+@import url("https://fonts.googleapis.com/css2?family=Staatliches&display=swap");
+@import url("https://fonts.googleapis.com/css2?family=Nanum+Pen+Script&display=swap");
+
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+body,
+html {
+  height: 100vh;
+  display: grid;
+  font-family: "Staatliches", cursive;
+  background: #d83565;
+  color: black;
+  font-size: 14px;
+  letter-spacing: 0.1em;
+}
+
+.ticket {
+  margin: 10px;
+  display: flex;
+  padding: 5px;
+  background: white;
+  border-radius: 10px;
+  box-shadow: rgba(0, 0, 0, 0.3) 0px 19px 38px,
+    rgba(0, 0, 0, 0.22) 0px 15px 12px;
+}
+
+.left {
+  display: flex;
+}
+.planet-img {
+  height: 250px;
+}
+
+.image {
+  height: 250px;
+  width: 250px;
+  // background-image: url("https://media.pitchfork.com/photos/60db53e71dfc7ddc9f5086f9/1:1/w_1656,h_1656,c_limit/Olivia-Rodrigo-Sour-Prom.jpg");
+  background-size: contain;
+  opacity: 0.85;
+}
+
+.sponsor-img {
+  height: 40px;
+  width: 40px;
+}
+
+.admit-one {
+  position: absolute;
+  color: darkgray;
+  height: 250px;
+  padding: 0 10px;
+  letter-spacing: 0.15em;
+  display: flex;
+  text-align: center;
+  justify-content: space-around;
+  writing-mode: vertical-rl;
+  //   transform: rotate(-180deg);
+}
+
+.admit-one span:nth-child(2) {
+  color: white;
+  font-weight: 700;
+}
+
+.left .ticket-number {
+  height: 250px;
+  width: 250px;
+  display: flex;
+  justify-content: flex-end;
+  align-items: flex-end;
+  padding: 5px;
+}
+
+.ticket-info {
+  padding: 10px 30px;
+  display: flex;
+  flex-direction: column;
+  text-align: center;
+  justify-content: space-between;
+  align-items: center;
+}
+
+.date {
+  border-top: 1px solid gray;
+  border-bottom: 1px solid gray;
+  padding: 5px 0;
+  font-weight: 700;
+  display: flex;
+  align-items: center;
+  justify-content: space-around;
+  color: #d83565;
+}
+
+.date span {
+  width: 100px;
+}
+
+.date span:first-child {
+  text-align: left;
+}
+
+.date span:last-child {
+  text-align: right;
+}
+
+.date .june-29 {
+  color: #d83565;
+  font-size: 20px;
+}
+
+.show-name {
+  font-size: 32px;
+  font-family: "Nanum Pen Script", cursive;
+  color: #d83565;
+}
+
+.show-name h1 {
+  font-size: 48px;
+  font-weight: 700;
+  letter-spacing: 0.1em;
+  color: #4a437e;
+}
+
+.time {
+  padding: 10px 0;
+  color: #4a437e;
+  text-align: center;
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
+  font-weight: 700;
+}
+
+.time span {
+  font-weight: 400;
+  color: gray;
+}
+
+.left .time {
+  font-size: 16px;
+}
+
+.location {
+  color: #d83565;
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  width: 100%;
+  padding-top: 8px;
+  border-top: 1px solid gray;
+}
+
+.location .separator {
+  font-size: 20px;
+}
+
+.right {
+  width: 180px;
+  border-left: 1px dashed #404040;
+}
+
+.right .admit-one {
+  color: darkgray;
+}
+
+.right .admit-one span:nth-child(2) {
+  color: gray;
+}
+
+.right .right-info-container {
+  height: 250px;
+  padding: 10px 10px 10px 35px;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-around;
+  align-items: center;
+}
+
+.right .show-name h1 {
+  font-size: 18px;
+}
+
+.barcode {
+  height: 100px;
+}
+
+.barcode img {
+  height: 100%;
+}
+
+.right .ticket-number {
+  color: gray;
+}
 
 .tickets {
   position: absolute;
   right: 10px;
-  top: 400px;
+  top: 300px;
   background-color: var(--primary);
   border-radius: var(--radius);
-  padding: 10px 200px 10px 200px;
+  padding: 10px 100px 10px 100px;
 }
 .options {
   position: absolute;
